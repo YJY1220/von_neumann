e58e7e6842378888f696b6d29d9f5cf34d68c3d8
YJY1220 <jiyeyu1220@gmail.com>
2022-03-09 18:48:36 +0900

    complete Lv.03

---
+++ yjy/Lv.03/Lv.03/10950_A+B-3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b, t;
+	scanf("%d\n", &t);
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+	
+}
